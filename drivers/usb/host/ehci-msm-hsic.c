f99c0ae79fbbfbb9ff2eb240f7c1e6ce87d7f6f1
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-09-14 14:02:36 +0530

    EHCI: HSIC: Fail root hub bus suspend when port is not enabled
    HSIC controller can not detect HSIC device in low power mode.  Hence
    controller suspend is allowed only when port is enabled.  But this
    check is done in controller suspend routine.  Controller suspend is
    attempted after root hub bus is suspended.  Even if we fail, controller
    suspend, root hub bus remains in suspend state.  This would delay
    the enumeration of HSIC device as root hub bus resume takes some time.
    
    CRs-Fixed: 396444
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    (cherry picked from commit a723f744025854c7132410fd1699bc0997617fb9)
    
    Change-Id: I30c2117a0a6debfbc98a08610a83b56d5ac75f5b
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>
    
    Conflicts:
    	drivers/usb/host/ehci-msm-hsic.c
    modified for HTC M7
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -1047,12 +1047,6 @@ static int msm_hsic_suspend(struct msm_hsic_hcd *mehci)
 		return 0;
 	}
 
-	if (!(readl_relaxed(USB_PORTSC) & PORT_PE)) {
-		dev_err(mehci->dev, "%s:port is not enabled skip suspend\n",
-				__func__);
-		return -EAGAIN;
-	}
-
 	disable_irq(hcd->irq);
 
 	
@@ -1363,6 +1357,15 @@ static int ehci_hsic_urb_enqueue(struct usb_hcd *hcd, struct urb *urb,
 
 static int ehci_hsic_bus_suspend(struct usb_hcd *hcd)
 {
+	struct msm_hsic_hcd *mehci = hcd_to_hsic(hcd);
+
+	if (!(readl_relaxed(USB_PORTSC) & PORT_PE)) {
+		dbg_log_event(NULL, "RH suspend attempt failed", 0);
+		dev_dbg(mehci->dev, "%s:port is not enabled skip suspend\n",
+				__func__);
+		return -EAGAIN;
+	}
+
 	dbg_log_event(NULL, "Suspend RH", 0);
 	pr_info("%s: Suspend RH\n", __func__);
 	return ehci_bus_suspend(hcd);
