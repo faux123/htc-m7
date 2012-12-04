1323b4d7b3f18d991def1cb56e21feb6a13a3230
Tatyana Brokhman <tlinder@codeaurora.org>
2012-12-04 16:04:15 +0200

    block: Add API for urgent request handling
    This patch add support in block & elevator layers for handling
    urgent requests. The decision if a request is urgent or not is taken
    by the scheduler. Urgent request notification is passed to the underlying
    block device driver (eMMC for example). Block device driver may decide to
    interrupt the currently running low priority request to serve the new
    urgent request. By doing so READ latency is greatly reduced in read&write
    collision scenarios.
    
    Note that if the current scheduler doesn't implement the urgent request
    mechanism, this code path is never activated.
    
    Change-Id: I8aa74b9b45c0d3a2221bd4e82ea76eb4103e7cfa
    Signed-off-by: Tatyana Brokhman <tlinder@codeaurora.org>
    
    Conflicts:
    	block/blk-core.c
    	block/blk-settings.c
    	block/blk.h
    	block/elevator.c
    modified for HTC M7
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- block/blk.h
+++ block/blk.h
@@ -34,6 +34,7 @@ void __generic_unplug_device(struct request_queue *);
 
 enum rq_atomic_flags {
 	REQ_ATOM_COMPLETE = 0,
+	REQ_ATOM_URGENT = 1,
 };
 
 static inline int blk_mark_rq_complete(struct request *rq)
@@ -46,6 +47,19 @@ static inline void blk_clear_rq_complete(struct request *rq)
 	clear_bit(REQ_ATOM_COMPLETE, &rq->atomic_flags);
 }
 
+static inline int blk_mark_rq_urgent(struct request *rq)
+{
+	return test_and_set_bit(REQ_ATOM_URGENT, &rq->atomic_flags);
+}
+
+static inline void blk_clear_rq_urgent(struct request *rq)
+{
+	clear_bit(REQ_ATOM_URGENT, &rq->atomic_flags);
+}
+
+/*
+ * Internal elevator interface
+ */
 #define ELV_ON_HASH(rq)		(!hlist_unhashed(&(rq)->hash))
 
 void blk_insert_flush(struct request *rq);
