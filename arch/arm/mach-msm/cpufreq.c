9c1678e714d83ffad1d3eb9d56214acfa0966c35
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2013-05-10 09:38:51 -0600

    msm: cpufreq: Configure WQ for higer priority
    When the workqueue runs at a lower priority, it gets starved by higher
    priority threads. Bump the WQ priority to high to ensure cpufreq work
    queue gets a fair scheduling chance
    
    Change-Id: I9e994da94a347dceb884e72ec3dd3da468a4471d
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -462,7 +462,8 @@ static int __init msm_cpufreq_register(void)
 	}
 
 #ifdef CONFIG_SMP
-	msm_cpufreq_wq = create_workqueue("msm-cpufreq");
+	msm_cpufreq_wq = alloc_workqueue("msm-cpufreq",
+			WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
 #endif
 
 	register_pm_notifier(&msm_cpufreq_pm_notifier);
