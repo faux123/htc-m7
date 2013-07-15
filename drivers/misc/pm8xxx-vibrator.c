41cb8f6fb285d39b9f452aa20d04a53550f28868
tbalden <illespal@gmail.com>
2013-07-15 14:43:59 -0500

    s3k: s2w/l2w/l2m/l2s patch-up for new kernel
    Signed-off-by: Paul Reioux <faux123@localhost.localdomain>

--- drivers/misc/pm8xxx-vibrator.c
+++ drivers/misc/pm8xxx-vibrator.c
@@ -195,6 +195,11 @@ retry:
 	}
 }
 
+int vibrate(int time)
+{
+    pm8xxx_vib_enable(&vib_dev->timed_dev, time);
+    return 0;
+}
 
 static int pm8xxx_vib_get_time(struct timed_output_dev *dev)
 {
--- include/linux/synaptics_i2c_rmi.h
+++ include/linux/synaptics_i2c_rmi.h
@@ -170,5 +170,12 @@ enum {
 	FUNCTION
 };
 
+extern uint8_t touchscreen_is_on(void);
+
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+/* Sweep2Wake */
+extern void sweep2wake_setdev(struct input_dev * input_device);
+#endif
+
 extern uint8_t getPowerKeyState(void);
 #endif 
