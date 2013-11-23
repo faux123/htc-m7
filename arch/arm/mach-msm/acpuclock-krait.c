684e5269b1d7cbc0ea7f0ba4b9f980c2b4bff93f
Paul Reioux <reioux@gmail.com>
2013-11-23 12:22:47 -0600

    Low CPU Clocks: Allow ultra low CPU frequencies for better battery performance
    while screen is on or background operations like audio books and music
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -38,6 +38,12 @@
 #include "acpuclock-krait.h"
 #include "avs.h"
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE			39
+#else
+#define FREQ_TABLE_SIZE			35
+#endif
+
 #define CPU_FOOT_PRINT_MAGIC				0xACBDFE00
 static void set_acpuclk_foot_print(unsigned cpu, unsigned state)
 {
@@ -940,7 +946,7 @@ void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
 #endif	/* CONFIG_CPU_VOTALGE_TABLE */
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
