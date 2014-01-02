56d0d74bfee4fc0526ba9b572b52cd5e4810269f
Paul Reioux <reioux@gmail.com>
2014-01-01 23:24:01 -0800

    fastcharge: initial adaptation for HTC One
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -53,6 +53,10 @@
 #include <mach/board.h>
 #include <mach/board_htc.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1290,6 +1294,11 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 		dev_err(motg->phy.dev,
 			"Failed notifying %d charger type to PMIC\n",
 							motg->chg_type);
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0)
+		mA = IDEV_ACA_CHG_MAX;
+#endif
 
 	if (motg->cur_power == mA)
 		return;
