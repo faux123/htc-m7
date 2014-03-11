b429afa90a3cb099907edc8e3663a58ddf501ef6
Pradeep Sawlani <sawlani@amazon.com>
2014-03-10 22:53:58 -0700

    ksm: check and skip page, if it is already scanned
    From: Pradeep Sawlani <sawlani@amazon.com>
    
    On system like Android where most of the process are forked
    from parent w/o execve, KSM can scan same page multiple times
    in one scan cycle. There is no advantage in scanning same page
    multiple times for merging. During testing with Android, it was
    observed around 60% pages are skipped for each scan cycle.
    
    Change-Id: I0cf01802f0b4d61fcab92558beb9e1c660dc9a77
    Link: http://lkml.kernel.org/r/CAMrOTPgBtANS_ryRjan0-dTL97U7eRvtf3dCsss=Kn+Uk89fuA@mail.gmail.com
    Signed-off-by: Pradeep Sawlani <sawlani@amazon.com>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	include/linux/page-flags.h
    	mm/ksm.c
    
    Conflicts:
    	include/linux/page-flags.h
    	mm/Kconfig

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -51,10 +51,15 @@ enum pageflags {
 #ifdef CONFIG_HTC_DEBUG_REPORT_MEMINFO
 	PG_kmalloc,			
 	PG_kgsl,
+#ifdef CONFIG_KSM_CHECK_PAGE
+	PG_ksm_scan0,		/* page has been scanned by even KSM cycle */
 #endif
 	__NR_PAGEFLAGS,
-
-	
+#ifdef CONFIG_KSM_CHECK_PAGE
+	/* page has been scanned by odd KSM cycle */
+	PG_ksm_scan1 = PG_owner_priv_1,
+#endif
+	/* Filesystems */
 	PG_checked = PG_owner_priv_1,
 
 	PG_fscache = PG_private_2,	
@@ -146,6 +151,10 @@ PAGEFLAG(Reserved, reserved) __CLEARPAGEFLAG(Reserved, reserved)
 PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
+#ifdef CONFIG_KSM_CHECK_PAGE
+CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
+CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
+#endif
 
 PAGEFLAG(Private, private) __SETPAGEFLAG(Private, private)
 	__CLEARPAGEFLAG(Private, private)
